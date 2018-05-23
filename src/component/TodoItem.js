8e00312f6be313b81d22917112658e8dc47f29bb
huangzhengchao <1014849336@qq.com>
2018-05-23 18:19:58 +0800

    first commit

---
+++ src/component/TodoItem.js
@@ -0,0 +1,15 @@
+import React, { Component,PropTypes } from 'react';
+
+
+export default class TodoItem extends Component{
+    render(){
+        const{todo}=this.props
+        return(
+            <li>
+                <div className="view">
+                    <label>{todo.text}</label>
+                </div>
+            </li>
+        )
+    }
+}
